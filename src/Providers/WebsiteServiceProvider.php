22814d7a635cca2dade19eeb184b1b7588b8cd93
wgaurav7850 <51202296+wgaurav7850@users.noreply.github.com>
2020-06-08 13:19:02 +0530

    Update WebsiteServiceProvider.php

--- src/Providers/WebsiteServiceProvider.php
+++ src/Providers/WebsiteServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 class WebsiteServiceProvider extends ServiceProvider {
 
     public function boot(){
-        
+        dd("We are loading our package service provider.");
     }
 
    public function register()
