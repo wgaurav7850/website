b5b6567a21b862096596aefd4c3147aec0f3f9fa
wgaurav7850 <51202296+wgaurav7850@users.noreply.github.com>
2020-06-08 13:09:20 +0530

    first commit

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Gaurav\Website\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider {
+
+    public function boot(){
+        
+    }
+
+   public function register()
+   {
+       
+   }
+}
+
+
+
+
+
+?>
